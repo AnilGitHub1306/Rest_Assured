6f99884f266e3db8e71706815d04ca8f51f08754
AnilGitHub1306 <126259261+AnilGitHub1306@users.noreply.github.com>
2024-02-29 13:17:25 +0530

    Update GET.java

--- src/httpRequest/GET.java
+++ src/httpRequest/GET.java
@@ -2,7 +2,9 @@ package httpRequest;
 
 public class GET 
 {
-public static void main(String[] args) {
-	
-}
+public static void main(String[] args) 
+	{
+	System.out.println("This is GET request");
+
+	}
 }
